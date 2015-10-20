5aa836faa532d8967c4d85d86d8108041458e1c9
Romain Bessuges <romainbessuges@gmail.com>
2015-10-20 11:48:12 +0200

    use imgix for img proxy, only if image

--- routes/api/v1/upload.js
+++ routes/api/v1/upload.js
@@ -3,6 +3,7 @@ var getRawBody = require('raw-body');
 var config = require('../../../config');
 var AWS = require('aws-sdk');
 var shortid = require('shortid');
+var isImage = require('is-image');
 
 AWS.config.update(config.s3);
 
@@ -14,18 +15,18 @@ router.post('/upload', function (req, res) {
         var ext = filename.substring(filename.lastIndexOf('.'));
         var id = shortid.generate();
         var s3obj = new AWS.S3({params: {Bucket: 'campsi-eu', Key: id + ext}});
-
-        s3obj.upload({Body: buf})
-            .on('httpUploadProgress', function (evt) {
-                    console.log(evt);
-                })
-            .send(function (err, data) {
-                      console.log(err);
-                      console.dir(data);
-                      res.json({
-                          uri: config.imgix.host + '/' + id + ext
-                      })
-                  });
+        var isImage =
+            s3obj.upload({Body: buf})
+                .on('httpUploadProgress', function (evt) {
+                        console.log(evt);
+                    })
+                .send(function (err, data) {
+                          console.log(err);
+                          console.dir(data);
+                          res.json({
+                              uri: isImage(id + ext) ? config.imgix.host + '/' + id + ext : data.Location
+                          });
+                      });
     });
 });
 
