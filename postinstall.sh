7dc9ceecaee493731be9a69cc943bd649adf89ca
Romain Bessuges <romainbessuges@gmail.com>
2015-10-21 14:50:48 +0200

    rm only if exist

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-rm ./node_modules/array-diff
+rm -f ./node_modules/array-diff
 ln -s ../lib/array-diff ./node_modules
-rm ./node_modules/campsi
+rm -f ./node_modules/campsi
 ln -s ../lib/campsi ./node_modules
-rm ./node_modules/cheerio-or-jquery
+rm -f ./node_modules/cheerio-or-jquery
 ln -s ../lib/cheerio-or-jquery ./node_modules
-rm ./public/components
+rm -f ./public/components
 ln -s ../lib/components ./public
 ./node_modules/bower/bin/bower install
