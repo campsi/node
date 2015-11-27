18f7f774c963e858c800d3ce439dc7a84ce835e5
romain <romainbessuges@gmail.com>
2015-11-27 22:29:27 +0100

    Remove serve-favicon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
