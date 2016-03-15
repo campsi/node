d4c8df00595107caf63357bcedc4e1d1a34fe07c
romainbessugesmeusy <romainbessuges@gmail.com>
2016-03-15 11:20:49 +0100

    test post install

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -f ./public/components
 ln -s ../lib/components ./public
-ln -s ../lib/campsi-components ./public
+ln -s ../lib/campsi-components ./public/campsi
 ./node_modules/bower/bin/bower install
