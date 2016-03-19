23b35aac70d9660eba63646d22dc30e896eaec5f
romainbessugesmeusy <romainbessuges@gmail.com>
2016-03-19 22:26:48 +0100

    code style

--- browser-config.js
+++ browser-config.js
@@ -1,10 +1,10 @@
 var deepcopy = require('deepcopy');
 var browserConfig = deepcopy(require('./config'));
 
-delete browserConfig['mongo_uri'];
-delete browserConfig['session_secret'];
-delete browserConfig['sendgrid_api_key'];
-delete browserConfig['s3'];
-delete browserConfig.auth0['clientSecret'];
+delete browserConfig.mongo_uri;
+delete browserConfig.session_secret;
+delete browserConfig.sendgrid_api_key;
+delete browserConfig.s3;
+delete browserConfig.auth0.clientSecret;
 
 module.exports =  browserConfig;
